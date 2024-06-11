ddd44f1470b8cd982c6298c74fc84fd3910bae19
DevSahil12 <140622058+DevSahil12@users.noreply.github.com>
2024-06-11 18:29:07 +0530

    Update MainActivity.java

--- src/main/java/com/master/newsapi/MainActivity.java
+++ src/main/java/com/master/newsapi/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -20,13 +21,15 @@ import com.master.newsapi.LoginActivities.Login;
 import com.master.newsapi.LoginActivities.SessionManager;
 import com.master.newsapi.Retrofit.ApiClient;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String API_KEY = "846a2356e7254f38a6cd07bc8af86226";
     private RecyclerView recyclerView;
@@ -42,93 +45,102 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar=findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        drawerLayout=findViewById(R.id.drawer_layout);
-        navigationView=findViewById(R.id.nav_view);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        toggle=new ActionBarDrawerToggle(this,
-                drawerLayout,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        sessionManager=new SessionManager(this);
-
+        sessionManager = new SessionManager(this);
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+       newsAdapter=new NewsAdapter(new ArrayList<>());
+        recyclerView.setAdapter(newsAdapter);
 
-
-        if (!sessionManager.isLoggedIn()){
+        if (!sessionManager.isLoggedIn()) {
             navigateToLogin();
         }
 
+        fetchNews();
+    }
 
-        NewsApiService apiService = ApiClient.getService().create(NewsApiService.class);
+    private void fetchNews() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://newsapi.org/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        Call<NewsResponse> call = apiService.getTopHeadlines("us", API_KEY);
+        NewsApiService newsApiService = retrofit.create(NewsApiService.class);
+
+        String query = "tesla";
+        String fromDate = "2024-05-11";
+        String sortBy = "publishedAt";
+        int page = 5;
+
+        Call<NewsResponse> call = newsApiService.getEverything(query, fromDate, sortBy, API_KEY, page);
         call.enqueue(new Callback<NewsResponse>() {
             @Override
-            public void onResponse(Call<NewsResponse> call,
-                                   Response<NewsResponse> response) {
+            public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
                 if (response.isSuccessful() && response.body() != null) {
-                    List<Article> articles = response.body().getArticles();
-                    newsAdapter = new NewsAdapter(articles);
-                    newsAdapter.setOnItemClickListener(new NewsAdapter.OnItemClickListener(){
+                    newsAdapter.setNewsList(response.body().getArticles());
+                    newsAdapter.setOnItemClickListener(new NewsAdapter.OnItemClickListener() {
                         @Override
-                        public void onItemClick(Article article){
+                        public void onItemClick(Article article) {
                             openNewsDetails(article.getUrl());
                         }
                     });
-                    recyclerView.setAdapter(newsAdapter);
                 } else {
-                    Log.e("MainActivity", "Response not successful");
+                    // Handle the error response here
+                    Toast.makeText(MainActivity.this, "Failed to fetch news", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(Call<NewsResponse> call, Throwable t) {
-                Log.e("MainActivity", "Error: " + t.getMessage());
+                // Handle the failure here
+                Toast.makeText(MainActivity.this, "An error occurred", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-
-
-    public void openNewsDetails(String url){
-        Intent intent=new Intent(MainActivity.this, NewsActivity.class);
-        intent.putExtra("url",url);
+    public void openNewsDetails(String url) {
+        Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+        intent.putExtra("url", url);
         startActivity(intent);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int id=item.getItemId();
-        if (id==R.id.nav_profile){
-            Intent intent=new Intent(MainActivity.this, ProfileActivity.class);
+        int id = item.getItemId();
+        if (id == R.id.nav_profile) {
+            Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
             startActivity(intent);
-
-        }else if (id==R.id.nav_logout){
-            sessionManager.setLogin(false,null);
+        } else if (id == R.id.nav_logout) {
+            sessionManager.setLogin(false, null);
             navigateToLogin();
         }
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
-    public void navigateToLogin(){
-        Intent intent=new Intent(MainActivity.this, Login.class);
+
+    public void navigateToLogin() {
+        Intent intent = new Intent(MainActivity.this, Login.class);
         startActivity(intent);
         finish();
     }
+
     @Override
-    public void onBackPressed(){
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
-        }else
+        } else
             super.onBackPressed();
     }
 }
