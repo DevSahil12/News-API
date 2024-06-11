bec1afbebf32fb551b5071cd3ccba3356381937c
DevSahil12 <140622058+DevSahil12@users.noreply.github.com>
2024-06-11 18:30:31 +0530

    Update NewsApiService.java

--- src/main/java/com/master/newsapi/Api/NewsApiService.java
+++ src/main/java/com/master/newsapi/Api/NewsApiService.java
@@ -1,16 +1,17 @@
 package com.master.newsapi.Api;
+
 import com.master.newsapi.NewsResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-
 public interface NewsApiService {
-    @GET("v2/top-headlines")
-    Call<NewsResponse> getTopHeadlines(
-            @Query("country") String country,
-            @Query("apiKey") String apiKey
+    @GET("v2/everything")
+    Call<NewsResponse> getEverything(
+            @Query("q") String query,
+            @Query("from") String from,
+            @Query("sortBy") String sortBy,
+            @Query("apiKey") String apiKey,
+            @Query("page") int page
     );
 }
-
-
