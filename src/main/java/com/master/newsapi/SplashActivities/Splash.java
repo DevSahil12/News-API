d870f0984c3bc3a17bb3a50981c927c90283acca
DevSahil12 <140622058+DevSahil12@users.noreply.github.com>
2024-06-11 18:27:33 +0530

    Update Splash.java

--- src/main/java/com/master/newsapi/SplashActivities/Splash.java
+++ src/main/java/com/master/newsapi/SplashActivities/Splash.java
@@ -7,23 +7,33 @@ import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.master.newsapi.LoginActivities.Login;
+import com.master.newsapi.MainActivity;
 import com.master.newsapi.R;
-
+import com.master.newsapi.LoginActivities.SessionManager;
 
 public class Splash extends AppCompatActivity {
+
+    private SessionManager sessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        sessionManager = new SessionManager(this);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash.this, Login.class);
-                startActivity(intent);
+                if (sessionManager.isLoggedIn()) {
+                    Intent intent = new Intent(Splash.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(Splash.this, Login.class);
+                    startActivity(intent);
+                }
                 finish();
             }
-        }, 4650);
+        }, 4000); 
     }
 }
